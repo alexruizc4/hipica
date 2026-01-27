521bba0fe6d68ad1487bc1ddd6db579cb0d38a53
alexruizc4 <alexruiz06132004@gmail.com>
2026-01-27 18:47:55 +0100

    prueba

---
+++ js/include.js
@@ -0,0 +1,18 @@
+
+function load(id, file) {
+  fetch(file)
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById(id).innerHTML = data;
+
+    if (id === "footer") {
+      const yearElement = document.getElementById("year");
+      if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+      }
+    }
+  });
+}
+
+load("header", "partials/header.html");
+load("footer", "partials/footer.html");
